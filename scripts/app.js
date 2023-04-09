873aa5a12df116cd8ea63938448d4c6b481710cf
fatima nahid <fatimanahid341@gmail.com>
2023-04-09 23:41:07 +0500

    commit

---
+++ scripts/app.js
@@ -0,0 +1,110 @@
+// 05 - 04 -2023
+// (alerts)//
+// alert("hello");
+// alert("hello_fatima");
+//var developer = prompt("hey! your name here.", "developer")
+// alert("hello");
+//console.log("developer");
+// (variables)//string*
+//var hellofatimaNahid_is_a_frontend_developer;
+//var hellofatimaNahid_is_a_frontend_developer =" fatima";
+// var  game = "Quiz";
+// var quizgame = "Quiz";
+// var x = "Quiz";
+// var name1 = "_fatima";
+// alert(name1);
+// var codeing=15;
+// codeing+16
+// var lastName = "Smith";
+// var cityOfOrigin = "New_Orleans";
+// var aussieGreeting = "g'Day";
+//var thanx = "Thanks for your input!"
+// alert(thanx);
+//(legal and illegal here)
+//var originalNum = 23;
+//var newNum = originalNum + 7;
+//var 1stPresident = "Washington";
+//alert(144);
+//(variable for numbers)
+//var caseQty = 135;
+//alert(caseQty);
+ // (numbers)//opreaters
+// var popularNumber = 4;
+// var popularNumber = 2 + 2;
+// alert(2 + 2);
+// var popularNumber = 12 - 24;
+// var popularNumber = 3 * 12;
+// var num = 10;
+// var popularNumber = num + 200;
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// var whatsLeftOver = 10 % 3;
+
+// (numbers)//actualy math expressions
+// var num = 1;
+// postfix increment
+// var newNum = num++;
+// num++;
+// num = num + 1;
+// postfix dicrement
+// num--;
+// var newnum = 1;
+// var newNum = --num;
+
+// var num = 1;
+// prefix increment
+// var newNum = ++num;
+// math function by (BODMAS)
+// ambiguity
+// var total = 1 + 3 * 4;
+// var totalCost = 1 + (3 * 4);
+// var totalCost = (1 + 3) * 4;
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// resultOfComputation = ((2 * 4) * 4) + 2;
+//(Concatenating)
+// var message = "Thanks for your input!";
+// alert(message);
+//alert("Thanks, " + userName + "!");
+//var message = "Thanks, ";
+//var banger = "!";
+//alert(message + userName + banger);
+//alert("Thanks, " + "Susan" + "!");
+// var message = "hello, ";
+// var userName = "fatima";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// alert("2" + "2");
+// alert("2 plus 2 equals " + 2 + 2);
+
+// (prompts)
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+// (prompts for numbers)
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+
+//  var num1 = prompt("num 1 please");
+//  var num2 = prompt =("num2 please");
+
+// console.log(num1+num2)
+// var num1 = prompt("num 1 please");
+//  var num2 = prompt =("num2 please");
+//  console.log (Number[num1+num2])
+// console.log(1+8)
+// console.log(1-8)
+// console.log(1*8)
+// console.log(1/8)
+// console.log(1%5)
+// console.log(5**9)
+
+//06-04-2023 
+//javascript TASK 1 
+var zakatPercentage = 0.025; // 2.5% as decimal //camel case
+var userInput = Number(prompt("Please enter your amount: "));
+var result = zakatPercentage * userInput;
+alert("Your zakat value is " + result);
